9f0296ad14509e020e26043d9656e1eb06ed3b8c
Girish Chaulagain <girishchaulagain5@gmail.com>
2025-02-01 13:45:25 +0545

    Created file structure

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/GirishChaulagain/dsa-queue-simulator
+
+go 1.23.5
---
+++ shared/module.go
@@ -0,0 +1,12 @@
+package shared
+
+type Vehicle struct {
+	Lane      string
+	VehicleId int
+	Direction string
+}
+
+type LaneInfo struct {
+	Lane  string
+	count int
+}
